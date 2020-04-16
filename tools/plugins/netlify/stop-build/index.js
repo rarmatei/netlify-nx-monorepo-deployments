7d6bc6926bc57d27ddffae92838aecf5d7a76841
Rares Matei <matei.rar@gmail.com>
2020-04-16 15:57:38 +0100

    update with affected command

--- tools/plugins/netlify/stop-build/index.js
+++ tools/plugins/netlify/stop-build/index.js
@@ -1,7 +1,13 @@
 module.exports = {
   onInit: ({ utils }) => {
-    const currentProject = 'bike-store';
-    const projectHasChanged = false;
+    const currentProject = process.env.PROJECT_NAME;
+    const lastDeployedCommit = process.env.CACHED_COMMIT_REF;
+    const latestCommit = 'HEAD';
+    const projectHasChanged = projectChanged(
+      currentProject,
+      lastDeployedCommit,
+      latestCommit
+    );
     if (!projectHasChanged) {
       utils.build.cancelBuild(
         `Build was cancelled because ${currentProject} was not affected by the latest changes`
@@ -9,3 +15,16 @@ module.exports = {
     }
   }
 };
+
+function projectChanged(currentProject, fromHash, toHash) {
+  const execSync = require('child_process').execSync;
+  const getAffected = `yarn --silent nx print-affected -- --base=${fromHash} --head=${toHash}`;
+  const output = execSync(getAffected).toString();
+  //get the list of changed projects from the output
+  const changedProjects = JSON.parse(output).projects;
+  if (changedProjects.find(project => project === currentProject)) {
+    return true;
+  } else {
+    return false;
+  }
+}
